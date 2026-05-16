662a409429b77af7042c2d8f52a9da1fe205f142
kris7011 <krisrl@hotmail.com>
2026-05-16 13:26:18 +0200

    Add Swagger UI

--- src/ShiftPlanner.Api/Program.cs
+++ src/ShiftPlanner.Api/Program.cs
@@ -10,15 +10,16 @@ using ShiftPlanner.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseGlobalExceptionHandling();
